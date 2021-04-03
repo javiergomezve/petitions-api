f5b17f6e64103c82f7ffb51a251176576971b777
Javier Gomez <javiergomezve@gmail.com>
2021-04-03 12:23:42 -0500

    [ADD] Author and Petitions rest functionality

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AuthorCollection;
+use App\Http\Resources\AuthorResource;
 use App\Models\Author;
 use Illuminate\Http\Request;
 
@@ -10,21 +12,21 @@ class AuthorController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AuthorCollection
      */
     public function index()
     {
-        //
+        return new AuthorCollection(Author::all());
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
+     * @param Author $author
+     * @return AuthorResource
      */
     public function show(Author $author)
     {
-        //
+        return new AuthorResource($author);
     }
 }
--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -2,63 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PetitionCollection;
+use App\Http\Resources\PetitionResource;
 use App\Models\Petition;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PetitionController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
+        return response()->json(new PetitionCollection(Petition::all()), Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return PetitionResource
      */
     public function store(Request $request)
     {
-        //
+        $petition = Petition::create($request->only([
+            'title', 'description', 'category', 'author', 'signees',
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Petition  $petition
-     * @return \Illuminate\Http\Response
+     * @param Petition $petition
+     * @return PetitionResource
      */
     public function show(Petition $petition)
     {
-        //
+        return new PetitionResource($petition);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Petition  $petition
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Petition $petition
+     * @return PetitionResource
      */
     public function update(Request $request, Petition $petition)
     {
-        //
+        $petition->update($request->only([
+            'title', 'description', 'category', 'author', 'signees',
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Petition  $petition
-     * @return \Illuminate\Http\Response
+     * @param Petition $petition
+     * @return JsonResponse
      */
     public function destroy(Petition $petition)
     {
-        //
+        $petition->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
